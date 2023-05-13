81a715a2412baf7e364d306a404456e1a7fa00f0
Saad Ansari <ansarisaad175@gmail.com>
2023-05-13 17:59:46 +0530

    Lines properly added

--- multiplayer.js
+++ multiplayer.js
@@ -7,8 +7,8 @@ class player {
 }
 let player1 = new player();
 let player2 = new player();
-
 var cells = [];
+var image = "red";
 function player_turn(a) {
   //player 1
   if (player1.turn == player2.turn) {
@@ -18,7 +18,11 @@ function player_turn(a) {
       "0 0 7px #f00,0 0 10px #f00,0 0 21px #f00,0 0 42px #fa0,0 0 82px #fa0,0 0 92px #fa0";
     player1.turn++;
     cells[a] = "X";
-    if (player1.turn >= 3) player1.won = decider("X");
+    if (player1.turn >= 3) player1.won = colorpicker("X");
+    if (player1.won) {
+      image = "red";
+      decider("X");
+    }
     document
       .getElementById("body")
       .animate(
@@ -35,7 +39,11 @@ function player_turn(a) {
       "0 0 7px #00f,0 0 10px #00f,0 0 21px #00f,0 0 42px #0fa,0 0 82px #0fa,0 0 92px #0fa";
     player2.turn++;
     cells[a] = "O";
-    if (player2.turn >= 3) player2.won = decider("O");
+    if (player2.turn >= 3) player2.won = colorpicker("O");
+    if (player2.won) {
+      image = "blue";
+      decider("O");
+    }
     document.getElementById("body").style.backgroundColor = "#6f92a3";
   }
   document.getElementById(a).setAttribute("disabled", "");
@@ -43,7 +51,7 @@ function player_turn(a) {
   if (player1.won) won("Player1");
   if (player2.won) won("Player2");
   if (player1.turn == 5) {
-    player1.won = decider("X");
+    player1.won = colorpicker("X");
     if (player1.won) won("Player1");
     else {
       document.getElementById("play").style.visibility = "visible";
@@ -53,42 +61,48 @@ function player_turn(a) {
     }
   }
 }
+function colorpicker(str) {
+  if (
+    (cells[0] == str && cells[1] == str && cells[2] == str) ||
+    (cells[3] == str && cells[4] == str && cells[5] == str) ||
+    (cells[6] == str && cells[7] == str && cells[8] == str) ||
+    (cells[0] == str && cells[3] == str && cells[6] == str) ||
+    (cells[1] == str && cells[4] == str && cells[7] == str) ||
+    (cells[2] == str && cells[5] == str && cells[8] == str) ||
+    (cells[0] == str && cells[4] == str && cells[8] == str) ||
+    (cells[2] == str && cells[4] == str && cells[6] == str)
+  ) {
+    return true;
+  }
+}
 function decider(str) {
   if (cells[0] == str && cells[1] == str && cells[2] == str) {
-    document.getElementById("imag").style.display = "inline-block";
-    document.getElementById("imag").style.top = "50px";
-    return true;
+    document.getElementById(image).style.display = "inline-block";
+    document.getElementById(image).style.top = "50px";
   } else if (cells[3] == str && cells[4] == str && cells[5] == str) {
-    document.getElementById("imag").style.top = "170px";
-    document.getElementById("imag").style.display = "inline-block";
-    return true;
+    document.getElementById(image).style.top = "170px";
+    document.getElementById(image).style.display = "inline-block";
   } else if (cells[6] == str && cells[7] == str && cells[8] == str) {
-    document.getElementById("imag").style.top = "290px";
-    document.getElementById("imag").style.display = "inline-block";
-    return true;
+    document.getElementById(image).style.top = "290px";
+    document.getElementById(image).style.display = "inline-block";
   } else if (cells[0] == str && cells[3] == str && cells[6] == str) {
-    document.getElementById("imag").style.transform = "rotate(90deg)";
-    document.getElementById("imag").style.left = "-115px";
-    document.getElementById("imag").style.display = "inline-block";
-    return true;
+    document.getElementById(image).style.transform = "rotate(90deg)";
+    document.getElementById(image).style.left = "-115px";
+    document.getElementById(image).style.display = "inline-block";
   } else if (cells[1] == str && cells[4] == str && cells[7] == str) {
-    document.getElementById("imag").style.transform = "rotate(90deg)";
-    document.getElementById("imag").style.left = "10px";
-    document.getElementById("imag").style.display = "inline-block";
-    return true;
+    document.getElementById(image).style.transform = "rotate(90deg)";
+    document.getElementById(image).style.left = "10px";
+    document.getElementById(image).style.display = "inline-block";
   } else if (cells[2] == str && cells[5] == str && cells[8] == str) {
-    document.getElementById("imag").style.transform = "rotate(90deg)";
-    document.getElementById("imag").style.left = "135px";
-    document.getElementById("imag").style.display = "inline-block";
-    return true;
+    document.getElementById(image).style.transform = "rotate(90deg)";
+    document.getElementById(image).style.left = "135px";
+    document.getElementById(image).style.display = "inline-block";
   } else if (cells[0] == str && cells[4] == str && cells[8] == str) {
-    document.getElementById("imag").style.transform = "rotate(45deg)";
-    document.getElementById("imag").style.display = "inline-block";
-    return true;
+    document.getElementById(image).style.transform = "rotate(45deg)";
+    document.getElementById(image).style.display = "inline-block";
   } else if (cells[2] == str && cells[4] == str && cells[6] == str) {
-    document.getElementById("imag").style.transform = "rotate(135deg)";
-    document.getElementById("imag").style.display = "inline-block";
-    return true;
+    document.getElementById(image).style.transform = "rotate(135deg)";
+    document.getElementById(image).style.display = "inline-block";
   }
 }
 function won(s) {
@@ -112,6 +126,7 @@ function playagain() {
     document.getElementById(index).disabled = false;
     document.getElementById(index).style.cursor = "pointer";
     document.getElementById(index).innerHTML = "";
+    document.getElementById(image).style.display = "none";
     cells.length = 0;
   }
   player1.turn = 0;
